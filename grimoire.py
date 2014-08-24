5a767175ab24087aafe1b7c74d35190fabaa9045
Anthony Scopatz <scopatz@gmail.com>
2014-08-23 19:38:45 -0500

    added more magic cones

--- grimoire.py
+++ grimoire.py
@@ -10,8 +10,8 @@ def cast(trans, player, spell, target=None):
     invs = inventories(trans)
     if player not in invs:
         raise ValueError('{0} is not playing magic cones, too bad!'.format(player))
-    if (target is not None) and (target not in invs):
-        raise ValueError('Target {0} is not playing magic cones!'.format(target))
+    #if (target is not None) and (target not in invs):
+    #    raise ValueError('Target {0} is not playing magic cones!'.format(target))
     inv = invs[player]
     if inv['magic'].get(spell, 0) == 0:
         raise ValueError('{0} does not have any {1} cones!'.format(player, spell))
@@ -58,4 +58,61 @@ def counter(trans, player, spell, target=None):
         i -= 1
 
 SPELLBOOK['counter'] = counter
+
+def clone(trans, player, spell, target=None):
+    """clones all of cones from target pull request and gives them to the caster.
+    This includes the bounty and the cones from all reviewers.
+    """
+    if target is None:
+        raise ValueError("clone cone requires a pull request number")
+    target = int(target)
+    n = 0
+    hist = trans['history']
+    for tran in hist:
+        if tran.get('pr', -1) != target:
+            continue
+        if tran.get('countered', False):
+            continue
+        if tran.get('kind', '') == 'bounty_poster': 
+            continue
+        n += tran.get('cones', 0)
+    hist.append({'player': player, 'kind': spell, 'cones': n, 'pr': target})
+
+SPELLBOOK['clone'] = clone
+
+def _common_xfer(frac, trans, player, spell, target=None):
+    hist = trans['history']
+    if target is None or target == player:
+        hist.append({'player': player, 'cones': 0, 'kind': spell, 
+                     'magic': {spell: -1}})
+    else:
+        invs = inventories(trans)
+        n = int(invs[player]['cones'] * frac)
+        hist.append({'player': player, 'cones': -n, 'kind': spell, 
+                     'magic': {spell: -1}, 'target': target})
+        hist.append({'player': target, 'cones': n, 'kind': spell, 'target': player})
+
+def feed(trans, player, spell, target=None):
+    """transfers 1/8 of the casters cones to the target."""
+    _common_xfer(0.125, trans, player, spell, target=target)
+
+SPELLBOOK['feed'] = feed
+
+def cede(trans, player, spell, target=None):
+    """transfers 1/4 of the casters cones to the target."""
+    _common_xfer(0.25, trans, player, spell, target=target)
+
+SPELLBOOK['cede'] = cede
+
+def deed(trans, player, spell, target=None):
+    """transfers 1/2 of the casters cones to the target."""
+    _common_xfer(0.5, trans, player, spell, target=target)
+
+SPELLBOOK['deed'] = deed
+
+def meed(trans, player, spell, target=None):
+    """transfers all of the casters cones to the target."""
+    _common_xfer(1.0, trans, player, spell, target=target)
+
+SPELLBOOK['meed'] = meed
 
--- tests.py
+++ tests.py
@@ -73,4 +73,79 @@ def test_cast_counter():
     invs = utils.inventories(trans)
     yield assert_equal, 1, invs['b']['cones']
 
+def test_cast_clone():
+    spell = 'clone'
+    hist = [{'player': 'a', 'cones': 0, 'magic': {spell: 1}},
+            {'player': 'b', 'cones': 1, 'pr': 42}, 
+            {'player': 'c', 'cones': 1, 'pr': 42}, 
+            ]
+    trans = {'history': hist}
+    grimoire.cast(trans, 'a', spell, target='42')
+    invs = utils.inventories(trans)
+    yield assert_equal, 2, invs['a']['cones']
+
+def test_cast_feed():
+    spell = 'feed'
+    hist = [{'player': 'a', 'cones': 100, 'magic': {spell: 1}}]
+    trans = {'history': hist}
+    grimoire.cast(trans, 'a', spell)
+    exp = {'player': 'a', 'cones': 0, 'kind': spell, 'magic': {spell: -1}}
+    yield assert_equal, exp, hist[-1]
+
+    hist.append({'player': 'b', 'cones': 100, 'magic': {spell: 1}})
+    grimoire.cast(trans, 'b', spell, target='a')
+    exp_b = {'player': 'b', 'cones': -12, 'kind': spell, 'magic': {spell: -1}, 
+             'target': 'a'}
+    exp_a = {'player': 'a', 'cones': 12, 'kind': spell, 'target': 'b'}
+    yield assert_equal, exp_b, hist[-2]
+    yield assert_equal, exp_a, hist[-1]
+
+def test_cast_cede():
+    spell = 'cede'
+    hist = [{'player': 'a', 'cones': 100, 'magic': {spell: 1}}]
+    trans = {'history': hist}
+    grimoire.cast(trans, 'a', spell)
+    exp = {'player': 'a', 'cones': 0, 'kind': spell, 'magic': {spell: -1}}
+    yield assert_equal, exp, hist[-1]
+
+    hist.append({'player': 'b', 'cones': 100, 'magic': {spell: 1}})
+    grimoire.cast(trans, 'b', spell, target='a')
+    exp_b = {'player': 'b', 'cones': -25, 'kind': spell, 'magic': {spell: -1}, 
+             'target': 'a'}
+    exp_a = {'player': 'a', 'cones': 25, 'kind': spell, 'target': 'b'}
+    yield assert_equal, exp_b, hist[-2]
+    yield assert_equal, exp_a, hist[-1]
+
+def test_cast_deed():
+    spell = 'deed'
+    hist = [{'player': 'a', 'cones': 100, 'magic': {spell: 1}}]
+    trans = {'history': hist}
+    grimoire.cast(trans, 'a', spell)
+    exp = {'player': 'a', 'cones': 0, 'kind': spell, 'magic': {spell: -1}}
+    yield assert_equal, exp, hist[-1]
+
+    hist.append({'player': 'b', 'cones': 100, 'magic': {spell: 1}})
+    grimoire.cast(trans, 'b', spell, target='a')
+    exp_b = {'player': 'b', 'cones': -50, 'kind': spell, 'magic': {spell: -1}, 
+             'target': 'a'}
+    exp_a = {'player': 'a', 'cones': 50, 'kind': spell, 'target': 'b'}
+    yield assert_equal, exp_b, hist[-2]
+    yield assert_equal, exp_a, hist[-1]
+
+def test_cast_meed():
+    spell = 'meed'
+    hist = [{'player': 'a', 'cones': 100, 'magic': {spell: 1}}]
+    trans = {'history': hist}
+    grimoire.cast(trans, 'a', spell)
+    exp = {'player': 'a', 'cones': 0, 'kind': spell, 'magic': {spell: -1}}
+    yield assert_equal, exp, hist[-1]
+
+    hist.append({'player': 'b', 'cones': 100, 'magic': {spell: 1}})
+    grimoire.cast(trans, 'b', spell, target='a')
+    exp_b = {'player': 'b', 'cones': -100, 'kind': spell, 'magic': {spell: -1}, 
+             'target': 'a'}
+    exp_a = {'player': 'a', 'cones': 100, 'kind': spell, 'target': 'b'}
+    yield assert_equal, exp_b, hist[-2]
+    yield assert_equal, exp_a, hist[-1]
+
 
